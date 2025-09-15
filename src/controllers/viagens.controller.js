0a8cec72a0ce865df0528ec53b2c5faeb3b5d000
Irani Junior <iranijunior95@gmail.com>
2025-09-14 21:36:27 -0300

    Complemento do CRUD de viagens

--- src/controllers/viagens.controller.js
+++ src/controllers/viagens.controller.js
@@ -1,4 +1,68 @@
 import Viagem from "../models/viagens.model.js";
+import { objectIdIsValid } from "../utils/isValidObjectId.js";
+
+async function searchAllTrips(req, res) {
+    try {
+        const listaViagens = await Viagem.find()
+            .populate("motorista")
+            .populate("veiculo")
+            .populate("ajudantes")
+
+        if(listaViagens.length === 0) {
+            return res.status(200).json({
+                status: true,
+                message: "Nenhuma viagem encontrada",
+                viagens: []
+            });
+        }
+
+        return res.status(200).json({
+            status: true,
+            message: "Viagens encontradas com sucesso",
+            viagens: listaViagens
+        });
+
+    } catch (error) {
+        console.log(`Erro ao buscar viagens: ${error}`);
+
+        return res.status(500).json({ 
+            status: false, 
+            message: "Erro interno ao buscar viagens"
+        });
+    }
+}
+
+async function searchForTripsById(req, res) {
+    const { id } = req.params;
+
+    try {
+        const viagem = await Viagem.findById(id)
+            .populate("motorista")
+            .populate("veiculo")
+            .populate("ajudantes")
+
+        if(!viagem) {
+            return res.status(404).json({
+                status: false,
+                message: "Viagem não localizada"
+            });
+        }
+
+        return res.status(200).json({
+            status: true,
+            message: "Viagem localizada com sucesso",
+            viagem
+        });
+
+    } catch (error) {
+        console.log(`Erro ao buscar viagem pelo ID: ${error}`);
+
+        return res.status(500).json({ 
+            status: false, 
+            message: "Erro interno ao buscar viagem pelo ID"
+        });
+    }
+}
 
 async function addNewTrip(req, res) {
     const { 
@@ -42,6 +106,112 @@ async function addNewTrip(req, res) {
     }
 }
 
+async function changeTrips(req, res) {
+    const { id } = req.params;
+    const { 
+        dataSaida, 
+        dataChegada,
+        diaria,
+        peso,
+        carregamentos,
+        cidades,
+        motorista,
+        veiculo,
+        ajudantes
+    } = req.body;
+
+    try {
+        if (!objectIdIsValid(id)) {
+            return res.status(400).json({
+                status: false,
+                message: "ID não localizado"
+            });
+        }
+
+        const viagemAlterada = await Viagem.findOneAndUpdate(
+            { _id: id },
+            {
+                dataSaida, 
+                dataChegada,
+                diaria,
+                peso,
+                carregamentos,
+                cidades,
+                motorista,
+                veiculo,
+                ajudantes
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+        .populate("motorista")
+        .populate("veiculo")
+        .populate("ajudantes");
+
+        if (!viagemAlterada) {
+            return res.status(404).json({
+                status: false,
+                message: "Viagem não encontrada"
+            });
+        }
+
+        return res.status(200).json({
+            status: true,
+            message: "Viagem alterada com sucesso",
+            viagem: viagemAlterada
+        });
+
+    } catch (error) {
+        console.log(`Erro ao alterar viagem: ${error}`);
+
+        return res.status(500).json({
+            status: false,
+            message: "Erro interno ao alterar viagem"
+        });
+    }
+}
+
+async function deleteTrips(req, res) {
+    const { id } = req.params;
+
+    try {
+        if (!objectIdIsValid(id)) {
+            return res.status(400).json({
+                status: false,
+                message: "ID não localizado"
+            });
+        }
+
+        const viagemDeletada = await Viagem.findOneAndDelete(id);
+
+        if (!viagemDeletada) {
+            return res.status(404).json({
+                status: false,
+                message: "Viagem não encontrada"
+            });
+        }
+
+        return res.status(200).json({
+            status: true,
+            message: "Viagem deletada com sucesso"
+        });
+
+    } catch (error) {
+        console.log(`Erro ao deletar viagem: ${error}`);
+
+        return res.status(500).json({
+            status: false,
+            message: "Erro interno ao deletar viagem"
+        });
+    }
+}
+
 export default {
-    addNewTrip
+    searchAllTrips,
+    searchForTripsById,
+    addNewTrip,
+    changeTrips,
+    deleteTrips
 }
--- src/routes/viagens/viagens.routes.js
+++ src/routes/viagens/viagens.routes.js
@@ -4,9 +4,26 @@ import viagensMiddleware from "../../middlewares/viagens.middleware.js";
 
 const viagensRouter = Router();
 
+viagensRouter.get('/viagens', 
+    viagensController.searchAllTrips
+);
+
+viagensRouter.get('/viagens/:id', 
+    viagensController.searchForTripsById
+);
+
 viagensRouter.post('/viagens',
     viagensMiddleware.validateTravelData, 
     viagensController.addNewTrip
+);
+
+viagensRouter.put('/viagens/:id', 
+    viagensMiddleware.validateTravelData,
+    viagensController.changeTrips
+);
+
+viagensRouter.delete('/viagens/:id', 
+    viagensController.deleteTrips
 );
 
 export default viagensRouter;
